fc98da715d977e982953ca5f4554aa88e114c04d
William Breen <wbreen@mooney202-17.elon.edu>
2016-11-07 14:50:37 -0500

    finished day 1 with zombie

--- Rollerball/GameScene.swift
+++ Rollerball/GameScene.swift
@@ -8,82 +8,55 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //going to create a "game" where tilting the phone moves a ball the 
+    
+    //once ball is made available: replace all instances of "zombie" with "ball"
+    //let ball = SKSpriteNode(imageNamed: "Ball")
+    let zombie = SKSpriteNode(imageNamed: "Zombie")
+    let motionManager = CMMotionManager()
+    
+    //let ballSpeed: CGFloat = 15.0
+    let zombieSpeed: CGFloat = 15.0
     
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.white
+        //ball.position = CGPoint( x: size.width * 0.1, y: size.height * 0.5)
+        zombie.position = CGPoint( x: size.width * 0.1, y: size.height * 0.5)
+        addChild(zombie)        //becomes ball in final project
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        motionManager.startAccelerometerUpdates()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        if let accelData = motionManager.accelerometerData {
+            
+            let newX = zombie.position.x - CGFloat(accelData.acceleration.y) * zombieSpeed      //switched x and y b/c they were reversed on the phone
+            let newY = zombie.position.y + CGFloat(accelData.acceleration.x) * zombieSpeed
+            
+            zombie.position.x = newX
+            zombie.position.y = newY
+            
+            let halfWidth = zombie.size.width / 2
+            let halfHeight = zombie.size.height / 2
+            
+            if zombie.position.x < halfWidth {
+                zombie.position.x = halfWidth
+            } else if zombie.position.x + halfWidth > size.width {
+                zombie.position.x = size.width - halfWidth
+            }
+            
+            if zombie.position.y < halfHeight {
+                zombie.position.y = halfHeight
+            } else if zombie.position.y + halfHeight > size.height {
+                zombie.position.y = size.width - halfHeight
+            }
+        }
+        
     }
+    
 }
